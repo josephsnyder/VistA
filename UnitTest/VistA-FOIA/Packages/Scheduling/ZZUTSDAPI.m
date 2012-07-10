829c70873dbc28dda90777465bda6bf6bb76f7d6
jason.li <jason.li@kitware.com>
2012-07-10 16:35:55 -0400

    first step of SDAPI unit testing

---
+++ UnitTest/VistA-FOIA/Packages/Scheduling/ZZUTSDAPI.m
@@ -0,0 +1,72 @@
+ZZUTSDAPI ;kitware/JNL - code for a unit test SDAPI tag;07/10/12  11:15
+ ;;1.0;UNIT TEST;;JUL 10, 2012;Build 1
+ ; makes it easy to run tests simply by running this routine and
+ ; insures that XTMUNIT will be run only where it is present
+ TSTART
+ I $T(EN^XTMUNIT)'="" D EN^XTMUNIT("ZZUTSDAPI")
+ TROLLBACK
+ Q
+ ;
+STARTUP ; optional entry point
+ ; if present executed before any other entry point any variables
+ ; or other work that needs to be done for any or all tests in the
+ ; routine.  This is run only once at the beginning.
+ Q
+ ;
+SHUTDOWN ; optional entry point
+ ; if present executed after all other processing is complete to remove
+ ; any variables, or undo work done in STARTUP.
+ Q
+ ;
+SETUP ; optional entry point
+ ; if present it will be executed before each test entry to set up
+ ; variables, etc.
+ Q
+ ;
+TEARDOWN ; optional entry point
+ ; if present it will be exceuted after each test entry to clean up
+ ; variables, etc.
+ Q
+ ;
+CHKRCODE   ; Unit test to test the return code of $$SDAPI^SDAMA301
+ ;
+ N SDARRAY,RCODE
+ ;first case is invalid date/time format, this should just return -1
+ S SDARRAY(1)=0
+ S SDARRAY("FLDS")="ALL"
+ S SDARRAY(2)=3
+ S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
+ S ERRMSG="Expected rcode is -1"
+ D CHKEQ^XTMUNIT(RCODE,-1,ERRMSG_" real: "_RCODE)
+ D CHKTF^XTMUNIT($D(^TMP($J,"SDAMA301",115))'=0,"Expecting Error Code is 115")
+ I RCODE K ^TMP($J,"SDAMA301")
+ ;second case is invalid patient id -1, this should just return -1
+ K SDARRAY
+ S RCODE=0
+ S SDARRAY(1)="3120710;3130710"
+ S SDARRAY("FLDS")="ALL"
+ S SDARRAY(4)=-1
+ S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
+ D CHKEQ^XTMUNIT(RCODE,-1,ERRMSG_" real: "_RCODE)
+ D CHKTF^XTMUNIT($D(^TMP($J,"SDAMA301",115))'=0,"Expecting Error Code is 115")
+ I RCODE K ^TMP($J,"SDAMA301")
+ ;third case is invalid clinic id -1, this should just return -1
+ K SDARRAY
+ S RCODE=0
+ S SDARRAY(1)="3120710;3130710"
+ S SDARRAY("FLDS")="ALL"
+ S SDARRAY(2)=-1
+ S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
+ D CHKEQ^XTMUNIT(RCODE,-1,ERRMSG_" real: "_RCODE)
+ D CHKTF^XTMUNIT($D(^TMP($J,"SDAMA301",115))'=0,"Expecting Error Code is 115")
+ I RCODE K ^TMP($J,"SDAMA301")
+ Q
+XTROU ;
+ ;;SDAMA301
+ ; Entry points for tests are specified as the third semi-colon piece,
+ ; a description of what it tests is optional as the fourth semi-colon
+ ; piece on a line. The first line without a third piece terminates the
+ ; search for TAGs to be used as entry points
+XTENT ;
+ ;;CHKRCODE; unit test to check return code of $$SDAPI^SDAMA301
+ Q
