40f0692d1f2d7297d92a1f59a802245ed36a847c
Jason Li <jason.li@kitware.com>
2012-07-11 14:16:48 -0400

    added a new ZZUTSDCOM file to hold consts
    Also modified the rest of test cases to use the consts defined
    at ZZUTSDCOM
    
    Change-Id: I92de926cb4332ac03a0833482032e6e553fb7bca

--- UnitTest/VistA-FOIA/Packages/Scheduling/ZZUTSDAPI.m
+++ UnitTest/VistA-FOIA/Packages/Scheduling/ZZUTSDAPI.m
@@ -11,11 +11,13 @@ STARTUP ; optional entry point
  ; if present executed before any other entry point any variables
  ; or other work that needs to be done for any or all tests in the
  ; routine.  This is run only once at the beginning.
+ D INIT^ZZUTSDCOM
  Q
  ;
 SHUTDOWN ; optional entry point
  ; if present executed after all other processing is complete to remove
  ; any variables, or undo work done in STARTUP.
+ D CLEANUP^ZZUTSDCOM
  Q
  ;
 SETUP ; optional entry point
@@ -30,11 +32,10 @@ TEARDOWN ; optional entry point
  ;
 CHKRCODE   ; Unit test to test the return code of $$SDAPI^SDAMA301
  ;
- N SDARRAY,RCODE
  ;first case is invalid date/time format, this should just return -1
- S SDARRAY(1)=0
+ S SDARRAY(1)=INVLDDT
  S SDARRAY("FLDS")="ALL"
- S SDARRAY(2)=3
+ S SDARRAY(2)=TESTCID1
  S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
  S ERRMSG="Expected rcode is -1"
  D CHKEQ^XTMUNIT(RCODE,-1,ERRMSG_" real: "_RCODE)
@@ -43,9 +44,9 @@ CHKRCODE   ; Unit test to test the return code of $$SDAPI^SDAMA301
  ;second case is invalid patient id -1, this should just return -1
  K SDARRAY
  S RCODE=0
- S SDARRAY(1)="3120710;3130710"
+ S SDARRAY(1)=TESTDRANG1
  S SDARRAY("FLDS")="ALL"
- S SDARRAY(4)=-1
+ S SDARRAY(4)=INVLDPID
  S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
  D CHKEQ^XTMUNIT(RCODE,-1,ERRMSG_" real: "_RCODE)
  D CHKTF^XTMUNIT($D(^TMP($J,"SDAMA301",115))'=0,"Expecting Error Code is 115")
@@ -53,16 +54,28 @@ CHKRCODE   ; Unit test to test the return code of $$SDAPI^SDAMA301
  ;third case is invalid clinic id -1, this should just return -1
  K SDARRAY
  S RCODE=0
- S SDARRAY(1)="3120710;3130710"
+ S SDARRAY(1)=TESTDRANG1
  S SDARRAY("FLDS")="ALL"
- S SDARRAY(2)=-1
+ S SDARRAY(2)=INVLDCID
  S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
  D CHKEQ^XTMUNIT(RCODE,-1,ERRMSG_" real: "_RCODE)
  D CHKTF^XTMUNIT($D(^TMP($J,"SDAMA301",115))'=0,"Expecting Error Code is 115")
  I RCODE K ^TMP($J,"SDAMA301")
  Q
+ ;fourth case is valid patient and valid clinic
+ K SDARRAY
+ S RCODE=0
+ S SDARRAY(1)=TESTDRANG2
+ S SDARRAY(2)=TESTCID1
+ S SDARRAY(4)=TESTPID1
+ S SDARRAY("FLD")="ALL"
+ S RCODE=$$SDAPI^SDAMA301(.SDARRAY)
+ D CHKEQ^XTMUNIT(RCODE,0,"Expected rcode 0, real: "_RCODE)
+ D CHKTF^XTMUNIT($D(^TMP($J,"SDAMA301"))=0, "^TMP global should be empty")
+ I RCODE K ^TMP($J,"SDAMA301")
 XTROU ;
  ;;SDAMA301
+ ;;ZZUTSDCOM
  ; Entry points for tests are specified as the third semi-colon piece,
  ; a description of what it tests is optional as the fourth semi-colon
  ; piece on a line. The first line without a third piece terminates the
